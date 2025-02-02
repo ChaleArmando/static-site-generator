7be975269f677b3014d22a800401180f8bcb2533
ChaleArmando <chalejesus216@gmail.com>
2025-02-02 15:01:28 -0500

    Add unittest shell and test for text nodes

---
+++ src/test_textnode.py
@@ -0,0 +1,30 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+    
+    def test_eq_url(self):
+        node = TextNode("This is a text node", TextType.BOLD, "www.boot.dev")
+        node2 = TextNode("This is a text node", TextType.BOLD, "www.boot.dev")
+        self.assertEqual(node, node2)
+        node3 = TextNode("Test", TextType.IMAGES, "www.google.com")
+        node4 = TextNode("Test", TextType.IMAGES, "www.yahoo.com")
+        self.assertNotEqual(node3, node4)
+
+    def test_none_url(self):
+        node = TextNode("This is a text node", TextType.BOLD, None)
+        node2 = TextNode("This is a text node", TextType.BOLD, None)
+        self.assertEqual(node, node2)
+
+    def test_not_eq_type(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a code node", TextType.CODE)
+        self.assertNotEqual(node, node2)
+
+if __name__ == "__main__":
+    unittest.main()
