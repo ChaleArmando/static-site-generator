f5f94ae55900b9e117ec5822c0f7f9b4250d9573
ChaleArmando <chalejesus216@gmail.com>
2025-02-08 14:30:01 -0500

    Add function to get Type of Block Markdown from Text and Unit Test

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,3 +1,5 @@
+import re
+
 def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")
     new_blocks = []
@@ -6,3 +8,22 @@ def markdown_to_blocks(markdown):
         if len(block) > 0:
             new_blocks.append(block)
     return new_blocks
+
+def block_to_block_type(block):
+    type = "paragraph"
+    if re.search(r'^#{1,6} .+', block): type = "heading"
+    if re.search(r'^(`{3})(\n*.*)+(`{3})$', block): type = "code"
+    if re.search(r'^((\n{0,1}\>.*)+)$',block): type = "quote"
+    if re.search(r'^((\n{0,1}[\*\-] .*)+)$', block): type = "unordered_list"
+    if re.search(r'^((\n{0,1}[0-9]+\. .*)+)$', block):
+        lines = block.split("\n")
+        for line in lines:
+            if line == "": lines.remove("")
+        count = 1
+        ordered_list = True
+        for line in lines:
+            order = re.search(r'^([0-9]+)', line)
+            if int(order.group()) != count: ordered_list = False
+            count += 1
+        if ordered_list: type = "ordered_list"
+    return type
